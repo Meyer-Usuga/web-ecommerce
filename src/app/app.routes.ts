1dced635c57a47672a0ab03d442142fa5909976a
Meyer-Usuga <usugarestrepomeyer@gmail.com>
2025-09-16 01:18:47 -0500

    changes: se actualizan las rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,15 +2,15 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: '',
-    loadComponent: () =>
-      import('@feature/pages/home').then((c) => c.HomeComponent),
-  },
-  {
     path: 'productos/:collection',
     loadComponent: () =>
       import('@feature/pages/product-list-page').then(
         (c) => c.ProductListPageComponent
       ),
+  },
+  {
+    path: '',
+    loadComponent: () =>
+      import('@feature/pages/home').then((c) => c.HomeComponent),
   },
 ];
